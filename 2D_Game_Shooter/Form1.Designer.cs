411bdf2e10bafba0ca5a47b76f18cd235a0785aa
Katsiaryna Dremliuh <nerona.91@gmail.com>
2022-07-19 23:38:58 +0300

    Add moving clouds

--- 2D_Game_Shooter/Form1.Designer.cs
+++ 2D_Game_Shooter/Form1.Designer.cs
@@ -28,20 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.MoveBigTimer = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
+            // 
+            // MoveBigTimer
+            // 
+            this.MoveBigTimer.Enabled = true;
+            this.MoveBigTimer.Interval = 10;
+            this.MoveBigTimer.Tick += new System.EventHandler(this.MoveBigTimer_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::_2D_Game_Shooter.Properties.Resources.Background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1264, 681);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "2D_Game_Shooter";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
-
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer MoveBigTimer;
     }
 }
 
--- 2D_Game_Shooter/Form1.cs
+++ 2D_Game_Shooter/Form1.cs
@@ -1,20 +1,72 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace _2D_Game_Shooter
 {
     public partial class Form1 : Form
     {
+        private PictureBox[] _clouds;
+        private int _backgroundSpeed;
+        private Random _random;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            const int countOfClouds = 20;
+
+            _backgroundSpeed = 5;
+            _clouds = new PictureBox[countOfClouds];
+            _random = new Random();
+
+            for (int i = 0; i < _clouds.Length; i++)
+            {
+                _clouds[i] = new PictureBox()
+                {
+                    BorderStyle = BorderStyle.None,
+                    Location = new Point(_random.Next(-1000, 1280), _random.Next(140, 320)),
+                };
+
+                if (i % 2 == 1)
+                {
+                    _clouds[i].Size = new Size(_random.Next(100, 255), _random.Next(30, 70));
+                    _clouds[i].BackColor = Color.FromArgb(_random.Next(50, 125), red: 255, green: 200, blue: 200);
+                }
+                else
+                {
+                    _clouds[i].Size = new Size(150, 25);
+                    _clouds[i].BackColor = Color.FromArgb(_random.Next(50, 125), red: 255, green: 205, blue: 205);
+                }
+
+                this.Controls.Add(_clouds[i]);
+            }
+        }
+
+        private void MoveBigTimer_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < _clouds.Length; i++)
+            {
+                _clouds[i].Left += _backgroundSpeed;
+
+                if (_clouds[i].Left >= this.Width) // Form1.Width = 1280
+                {
+                    _clouds[i].Left = _clouds[i].Height;
+                }
+            }
+
+            for (int i = _clouds.Length; i < _clouds.Length; i++)
+            {
+                _clouds[i].Left += _backgroundSpeed - 10;
+
+                if (_clouds[i].Left >= this.Width) // 1280
+                {
+                    _clouds[i].Left = _clouds[i].Left;
+                }
+            }
         }
     }
 }
